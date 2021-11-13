d03f4a44db1cd3160cdef7b0d11194aa18b76156
Isaac Joshua <isaacjcarnes@gmail.com>
2021-11-13 14:05:50 -0500

    Added server js with basic structure setup and get functions for index and notes html pages

---
+++ Develop/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
