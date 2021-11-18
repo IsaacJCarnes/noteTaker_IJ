102c57bef38520ef1296550de79a2cddd826b4df
Isaac Joshua <isaacjcarnes@gmail.com>
2021-11-17 22:44:19 -0500

    Backend for adding notes and getting notes is implemented, and notes are added with unique id's

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const PORT = process.env.port || 3001;
 
 const app = express();
 
+let lastIndex = null;
+
 app.use(express.static("public"));
 app.use(express.json());
 
@@ -24,8 +26,9 @@ app.get('/notes', (req, res) => {
 const readFromFile = (destination) => 
   fs.readFile(destination, 'utf8', (err, data) => {
     if (err) {
-      console.error("Reading " + err);
+      console.error(err);
     } else {
+
       return JSON.stringify(data);
     }
   });
@@ -40,31 +43,51 @@ const readAndAppend = (destination, content) => {
     if (err) {
       console.error(err);
     } else {
-      const parsedData = JSON.parse(data);
-      parsedData.push(content.req.body);
+      lastIndex++;
+      console.log(lastIndex);
+      let parsedData = JSON.parse(data);
+      let dataId = lastIndex + "+" + Math.floor(Math.random() * 999);
+      let newData = {
+        id: dataId,
+      };
+      Object.assign(newData, content.req.body);
+      parsedData.push(newData);
       writeToFile(destination, parsedData);
     }
   });
 };
 
-
 /*Functions in index.js*/
 //GET Route for notes data
 app.get('/api/notes', (req, res) => {
-  let notesFile = readFromFile(".\\db\\db.json");
-  console.log("no: "+ notesFile);
-  res.send(notesFile);
+  fs.readFile(".\\db\\db.json", 'utf8', (err, data) => {
+    if (err) {
+      console.error("Reading " + err);
+    } else {
+      res.send(data);
+    }
+  });  
 });
 
 // POST Route for notes data
 app.post('/api/notes', (req, res) => {
   readAndAppend('.\\db\\db.json', res);
+  res.json(`${req.method} request received to post to notes`);
 });
 
-app.delete('/api/notes', (req, res) => {
+app.delete('/api/notes/:id', (req, res) => {
+  console.log(req.params.id);
   res.json(`${req.method} request received to delete from notes`);
 });
 
-app.listen(PORT, () =>
+app.listen(PORT, () => {
+  fs.readFile(".\\db\\db.json", 'utf8', (err, data) => { //Read to get last note id
+    if (err) {
+      console.error(err);
+    } else {
+      let parsedData = JSON.parse(data);
+      lastIndex = Number(parsedData[parsedData.length-1].id.split('+',1)[0]);
+    }
+  });
   console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
+});
